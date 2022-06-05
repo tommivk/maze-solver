d1011517c2ffcd7aea3a830d38264fafc209ca17
tommivk <tommikokk@gmail.com>
2022-06-05 13:34:02 +0300

    add control buttons to UI

--- MazeSolver/app/src/main/java/mazesolver/UI/MazeSolverUI.java
+++ MazeSolver/app/src/main/java/mazesolver/UI/MazeSolverUI.java
@@ -5,37 +5,63 @@ import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import mazesolver.domain.Kruskal;
 import mazesolver.domain.Rect;
+import mazesolver.domain.Tremaux;
 import mazesolver.domain.WallFollower;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class MazeSolverUI extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Maze Solver");
-        stage.setHeight(1000);
-        stage.setWidth(1000);
-
-        GridPane pane = new GridPane();
-        pane.setAlignment(Pos.CENTER);
-
         Kruskal kruskal = new Kruskal();
         int size = 30;
-        Rect[][] rects = kruskal.generateEdges(size, size);
-        kruskal.generateMaze(false, 4);
+        Rect[][] maze = kruskal.generateEdges(size, size);
+        WallFollower wf = new WallFollower(maze);
+        Tremaux tremaux = new Tremaux(maze);
+
+        stage.setTitle("Maze Solver");
+        stage.setHeight(1200);
+        stage.setWidth(1400);
+
+        Button newMazeButton = new Button("generate maze");
+        newMazeButton.setOnMouseClicked(event -> {
+            kruskal.generateMaze(true, 4);
+        });
+
+        Button wfSolveButton = new Button("Wall follower");
+        wfSolveButton.setOnMouseClicked(event -> {
+            wf.solve();
+            wf.animate(wf.getMoves(), 30);
+        });
+
+        Button startTremaux = new Button("Tremaux's");
+        startTremaux.setOnMouseClicked(event -> {
+            tremaux.solve();
+        });
+
+        VBox navigation = new VBox();
+        navigation.getChildren().add(newMazeButton);
+        navigation.getChildren().add(wfSolveButton);
+        navigation.getChildren().add(startTremaux);
+        navigation.setPrefWidth(200);
+
+        GridPane pane = new GridPane();
+        pane.setAlignment(Pos.CENTER_RIGHT);
+
+        HBox mainContainer = new HBox();
+        mainContainer.getChildren().addAll(navigation, pane);
 
         for (int i = 0; i < size; i++) {
             for (int k = 0; k < size; k++) {
-                pane.add(rects[i][k].getRectangle(), i, k);
+                pane.add(maze[i][k].getRectangle(), i, k);
             }
         }
 
-        WallFollower wf = new WallFollower(rects);
-        wf.solve();
-        wf.animate(wf.getMoves(), 30);
-
-        Scene scene = new Scene(pane);
+        Scene scene = new Scene(mainContainer);
         stage.setScene(scene);
         stage.show();
     }
